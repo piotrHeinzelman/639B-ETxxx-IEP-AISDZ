8ca94591c17aaf27fec45df6ae7d16b5c9e76327
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2021-12-11 15:58:57 +0100

    proj 0 :: start

--- pre_proj_00/pre_proj_0/main.cpp
+++ pre_proj_00/pre_proj_0/main.cpp
@@ -2,8 +2,33 @@
 
 using namespace std;
 
-int main()
-{
+/*
+
+Algorytmy i Struktury Danych, zima 2021/2022
+Zadanie p0 Zwyk³¹ tablicê statyczn¹ P[n] (n -sta³a) wype³niæ wylosowanymi rekordami o znaczaj¹cymi wspó³rzêdne
+ca³kowite (x, y) punktów na p³aszczyŸniez zakresu <G, G>(G –sta³a).
+
+Nastêpnie:
+
+1. wydrukowaæ tablicê P.
+
+2. utworzyæ dwietablice dynamiczne zawieraj¹ce punkty le¿¹ce nad osi¹ x uk³adu wspó³rzêdnych
+   oraz pod osi¹ xuk³adu (a wiêc nie le¿¹ce na osiach uk³adu).
+
+3. wydrukowaæ tak utworzonetablice.
+
+Wskazówki:
+1. liczby ca³kowite z zakresu <d, g>, gdzie d i g s¹ liczbami ca³kowitymi,
+   losujemy jako d + rand() % (g+1-d).
+
+2. przed utworzeniem tablic dynamicznych i wpisaniem do nich rekordów z tablicy P trzeba najpierw policzyæ,
+   ile tych rekordów  bêdzie w ka¿dej tabli
+
+*/
+
+
+
+int main(){
     cout << "Hello world!" << endl;
     return 0;
 }
